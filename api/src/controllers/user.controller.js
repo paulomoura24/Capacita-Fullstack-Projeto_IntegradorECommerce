a68251f2f1a00980333198dbaabdca324034aaff
DESKTOP-13T158I\User <wesley.costa@ef5.com.br>
2025-09-17 01:29:02 -0300

    fix ajust code

--- api/src/controllers/user.controller.js
+++ api/src/controllers/user.controller.js
@@ -4,3 +4,9 @@ export async function me(req, res) {
   const user = await userService.getMe(req.user.sub);
   res.json({ user });
 }
+
+
+export async function list(req, res) {
+  const users = await userService.listUsers();
+  res.json({ users });
+}
--- api/src/routes/user.routes.js
+++ api/src/routes/user.routes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
-import { authRequired } from '../middlewares/auth.js';
+import { authRequired, requireRole } from '../middlewares/auth.js';
 import * as ctrl from '../controllers/user.controller.js';
 
 const r = Router();
 r.get('/me', authRequired, ctrl.me);
+r.get('/', authRequired, requireRole('ADMIN'), ctrl.list);
 export default r;
+
